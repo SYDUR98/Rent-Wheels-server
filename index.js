9d1a21e7746fbe2a7cb62ee82debecd11298084f
sydur <eng.sydur@gmail.com>
2025-11-10 18:33:33 +0600

    added mongodb and dotenv file

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 const port = process.env.PORT || 3000
 
 app.use(cors())
@@ -9,6 +11,39 @@ app.use(express.json())
 app.get('/', (req, res) => {
   res.send('server working!')
 })
+
+
+
+const DB_USER = process.env.DB_USER;
+const DB_PASS = process.env.DB_PASS;
+
+
+const uri = `mongodb+srv://${DB_USER}:${DB_PASS}@cluster0.chduhq7.mongodb.net/?appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 
 app.listen(port, () => {
   console.log(`rent-wheels-server runnig on port: ${port}`)
